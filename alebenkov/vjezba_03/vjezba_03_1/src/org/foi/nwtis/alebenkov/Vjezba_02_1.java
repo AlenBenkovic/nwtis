34206a7a761123190250a8ea545414eadaf91d14
benkabenko <alen.benkovic@foi.hr>
2016-03-24 21:18:04 +0100

    pocetak 3. zadace

---
+++ alebenkov/vjezba_03/vjezba_03_1/src/org/foi/nwtis/alebenkov/Vjezba_02_1.java
@@ -0,0 +1,83 @@
+package org.foi.nwtis.alebenkov;
+
+import org.foi.nwtis.alebenkov.kvadrati.*;
+
+public class Vjezba_02_1 {
+
+	public static void main(String args[]) {
+		if(args.length != 3) {
+			System.out.println("Broj argumenta ne odgovara");		
+			return;
+		}
+		
+		int odBroja = Integer.parseInt(args[0]);
+		int doBroja = Integer.parseInt(args[1]);
+
+		int vrsta = Integer.parseInt(args[2]);
+		Kvadrati kvad = null;
+		Ispisivac_1  ispisivac1 = null;
+		switch(vrsta) {
+		case 0:
+			kvad = new Kvadrati(odBroja, doBroja);
+			kvad.ispis();
+			break;
+		case 1:
+			kvad = new NeparniKvadrati_1(odBroja, doBroja);
+			kvad.ispis();
+			break;
+		case 2:
+			kvad = new NeparniKvadrati_2(odBroja, doBroja);
+			kvad.ispis();
+			break;
+		case 3:
+			kvad = new NeparniKvadrati_3(odBroja, doBroja);
+			kvad.ispis();
+			break;
+		case 4:
+			ispisivac1 = new NeparniKvadrati_3(odBroja, doBroja);
+			//ispisivac1.ispis();
+			ispisivac1.ispisiPodatke();
+			break;
+		case 5:
+			NeparniKvadrati_4 npk4 = new NeparniKvadrati_4(odBroja, doBroja);
+			npk4.ispisiPodatkeLinijski();
+			break;
+		case 6:
+			kvad = new NeparniKvadrati_4(odBroja, doBroja);
+			kvad.ispis();
+			break;
+		case 7:
+			ispisivac1 = new NeparniKvadrati_4(odBroja, doBroja);
+			ispisivac1.ispisiPodatke();
+			break;
+		case 8:
+			Ispisivac_2 ispisivac2 = new NeparniKvadrati_4(odBroja, doBroja);
+			ispisivac2.ispisiPodatkeLinijski();
+			break;
+		case 9:
+			NeparniKvadrati_2 np2 = new NeparniKvadrati_4(odBroja, doBroja);
+			np2.ispis();
+			break;
+		case 10:
+			for(int i=0;i<100;i++){
+				ispisivac1 = NeparniKvadrati_5
+					.kreirajIspisivac_1(odBroja, doBroja);
+				ispisivac1.ispisiPodatke();
+				System.out.println("Klasa je:"
+					+ispisivac1.getClass()+"\n");
+					
+				try{	
+					Thread.sleep(1000);
+				}catch(Exception e){
+					
+				}
+			}
+			break;
+		default:
+			System.out.println("Argumenti ne odgovaraju");
+		}
+			
+	}
+}
+		
+		
---
+++ alebenkov/vjezba_03/vjezba_03_1/src/org/foi/nwtis/alebenkov/kvadrati/NeparniKvadrati_2.java
@@ -0,0 +1,17 @@
+package org.foi.nwtis.alebenkov.kvadrati;
+
+public class NeparniKvadrati_2 extends Kvadrati {
+	
+	public NeparniKvadrati_2(int odBroja, int doBroja) {
+		super(odBroja,doBroja);
+	}
+	
+	public void ispis() {
+		int pocetak = this.odBroja % 2 == 0 ? this.odBroja +1 
+									: this.odBroja;
+		for(int i=pocetak;i <= this.doBroja;i+=4) {
+//			System.out.println(i + " * " + i + " = " + i*i);
+			System.out.printf("%3d * %3d = %3d\n", i, i, i*i);
+		}
+	}
+}
