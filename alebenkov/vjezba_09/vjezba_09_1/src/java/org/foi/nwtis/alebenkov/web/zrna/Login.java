c9b6c71cad472ea5d6db9b721e116f4c207fe623
benkabenko <alen.benkovic@foi.hr>
2016-05-13 18:38:00 +0200

    9ta gotova-ne radi sve

--- alebenkov/vjezba_09/vjezba_09_1/src/java/org/foi/nwtis/alebenkov/web/zrna/Login.java
+++ alebenkov/vjezba_09/vjezba_09_1/src/java/org/foi/nwtis/alebenkov/web/zrna/Login.java
@@ -7,6 +7,10 @@ package org.foi.nwtis.alebenkov.web.zrna;
 
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
 /**
  *
@@ -15,7 +19,7 @@ import javax.enterprise.context.RequestScoped;
 @Named(value = "login")
 @RequestScoped
 public class Login {
-    
+
     private String korisnickoIme;
     private String korisnickaLozinka;
 
@@ -44,14 +48,38 @@ public class Login {
         this.korisnickaLozinka = korisnickaLozinka;
         System.out.println("Korisnicko ime: " + korisnickaLozinka);
     }
-    
-    public String provjeraKorisnika(){
+
+    public String provjeraKorisnika() {
         //TODO provjeri korisnika prema
-        if(this.korisnickoIme.equals("alebenkov") && this.korisnickaLozinka.equals("123456")){
+        if (this.korisnickoIme.equals("alebenkov") && this.korisnickaLozinka.equals("123456")) {
             return "OK";
         } else {
             return "NOT_OK";
         }
     }
+
+    public void validirajKorisnickoIme(FacesContext facesContext, UIComponent arg1, Object value) throws ValidatorException {
+        String kIme = ((String) value).trim();
+        if (kIme.length() < 4 || kIme.length() > 10) {
+            FacesMessage message = new FacesMessage();
+            message.setSeverity(FacesMessage.SEVERITY_ERROR);
+            message.setSummary("Nispravan broj znakova. ");
+            message.setDetail(message.getSummary());
+            facesContext.addMessage("kIme", message);
+            throw new ValidatorException(message);
+        }
+    }
     
+     public void validirajKorisnickuLozinku(FacesContext facesContext, UIComponent arg1, Object value) throws ValidatorException {
+        String kLoz = ((String) value).trim();
+        if (kLoz.length() < 4 || kLoz.length() > 10) {
+            FacesMessage message = new FacesMessage();
+            message.setSeverity(FacesMessage.SEVERITY_ERROR);
+            message.setSummary("Nispravan broj znakova. ");
+            message.setDetail(message.getSummary());
+            facesContext.addMessage("kLoz", message);
+            throw new ValidatorException(message);
+        }
+    }
+
 }
